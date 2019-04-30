7894bf221073876cec01cbb8a0abbc69cdbbdd27
kth <katiehaster@gmail.com>
2019-04-29 21:03:35 -0700

    Updates

---
+++ precache-manifest.b9a18f820b73761e22b285cd940c0022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b270245f1f29dd3c64f1ea07c98b36ab",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ba1686aeb2c2f7e1cf1b",
+    "url": "/Clicky-Game/static/css/main.ddcecdd3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ba1686aeb2c2f7e1cf1b",
+    "url": "/Clicky-Game/static/js/main.1b8ca050.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
